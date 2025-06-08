147ec9fdde29be370a6c96c2c03707d2384b00b6
Priyanka Waychol <priyanka25waychol@gmail.com>
2025-06-08 09:56:31 +0530

    password.py

--- password.py
+++ password.py
@@ -1,19 +1,17 @@
 import random 
 import string
 
-def gen_password(length):
-    if length <4:
-        print("password length should be atleast 4 ")
-        return None
+def gen_password(length:int)->str:
+   
     
-    lowercase = string.ascii_lowercase
-    uppercase = string.ascii_uppercase
-    digits = string.digits
-    special_chars= string.punctuation
+    lowercase_chars = string.ascii_lowercase
+    uppercase_chars = string.ascii_uppercase
+    digits_chars = string.digits
+    punctuation_chars= string.punctuation
     
-    password_chars=[random.choice(lowercase),random.choice(uppercase),random.choice(digits),random.choice(special_chars)]
+    password_chars=[random.choice(lowercase_chars),random.choice(uppercase_chars),random.choice(digits_chars),random.choice(punctuation_chars)]
     
-    all_chars=lowercase + uppercase + digits + special_chars
+    all_chars=lowercase_chars + uppercase_chars + digits_chars+ punctuation_chars
     password_chars += random.choices(all_chars , k=length-4)
     
     random.shuffle(password_chars)
@@ -21,15 +19,22 @@ def gen_password(length):
 
 
 def main():
-    try:
-       length=int(input("Enter the password length(mininumm 4): "))
-    except ValueError:
-        print("Invalid input! Please enter a valid input as integer")
-        return
+    print("Password Generator")
+    while True:
+     try:
+         length_input=input("Enter the password length(mininumm 4): ")
+         length=int(length_input)
+         if length <4:
+          print("Please enter a password length should be atleast 4 ")
+          continue
+         break
+     except ValueError:
+        print("Invalid input! Please enter a valid  integer ")
+        
     
     password = gen_password(length)
-    if password:
-        print(f"Generated password: {password}")
+    print("\n Your Generated password is:")
+    print(password)
         
-if __name__== "__main__":
+if __name__== '__main__':
     main()
